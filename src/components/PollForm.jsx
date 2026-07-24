fb7fa520a933dd7105a829dcc69d086c2739f24d
Yangmei (Amy) Lu <109566558+AmyLuSunshine@users.noreply.github.com>
2026-07-24 19:28:01 -0400

    add delete button for Poll From and it's css

--- src/components/PollForm.jsx
+++ src/components/PollForm.jsx
@@ -1,126 +1,146 @@
-import { useState } from "react"
-import { useNavigate } from "react-router"
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
-function PollForm () {
-    const [polling, setPolling] = useState({
-        pollName: '',
-        pollDescription: ''
-    })
-    const [options, setOptions] = useState([{text: ""}, {text: ""}])
-    const [optionVal, setOptionVal] = useState('')
-    const [error, setError] = useState({
-        optionOne: "Option 1 is required",
-        optionTwo: "Option 2 is required",
-    })
-    const nav = useNavigate()
+function PollForm() {
+  const [polling, setPolling] = useState({
+    pollName: "",
+    pollDescription: "",
+  });
+  const [options, setOptions] = useState([{ text: "" }, { text: "" }]);
+  const [optionVal, setOptionVal] = useState("");
+  const [error, setError] = useState({
+    optionOne: "Option 1 is required",
+    optionTwo: "Option 2 is required",
+  });
+  const [deleteId, setDeleteId] = useState("");
 
-    function handleChange (e) {
-        setPolling((prevPolling) => {
-            return {
-                ...prevPolling,
-                [e.target.name]: e.target.value,
-            }
-        })
-    }
-    function handleOption(e) {
-        setOptions((prevOptions) => {
-            const inputIndex = Number(e.target.attributes["id"].value)
+  const nav = useNavigate();
 
-            return prevOptions.map((option, index) => {
-                if(index === inputIndex) {
-                    return {
-                        text: e.target.value
-                    }
-                } else {
-                    return {
-                        ...option
-                    }
-                }
-            })
-        })
-    }
-    async function handleSubmit(e) {
-        e.preventDefault()
-        if(options[0].text === "") {
-            alert(error.optionOne)
-            return;
-        } 
-        if(options[1].text === "") {
-            alert(error.optionTwo)
-            return;
+  function handleChange(e) {
+    setPolling((prevPolling) => {
+      return {
+        ...prevPolling,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+  function handleOption(e) {
+    setOptions((prevOptions) => {
+      const inputIndex = Number(e.target.attributes["id"].value);
+
+      return prevOptions.map((option, index) => {
+        if (index === inputIndex) {
+          return {
+            text: e.target.value,
+          };
+        } else {
+          return {
+            ...option,
+          };
         }
-
-        // check if option.text is empty, if it is remove it from the state
-
-
-
-        setOptions((prevOptions)=>{
-            return prevOptions.filter((option)=> {
-                if(Object.keys(option).length !== 0){
-                    return option
-                }
-            })
-        })
-
-        // const reviewedOptions = options.filter((option) => option.text.trim() !=="")
-        // console.log(reviewedOptions)
-        // setOptions(reviewedOptions)
-        console.log(options)
-
-        const response = await fetch('http://localhost:4000/polls', {
-            method: "POST",
-            body: JSON.stringify({title: polling.pollName, description: polling.pollDescription, options}),
-            headers: { "Content-Type": "application/json" }
-        })
-        const data = await response.json()
-
-        nav('/')
+      });
+    });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (options[0].text === "") {
+      alert(error.optionOne);
+      return;
     }
-    function addOption(e) {
-        e.preventDefault()
-        setOptions([...options, {}])
+    if (options[1].text === "") {
+      alert(error.optionTwo);
+      return;
     }
-    return (
-        <div>
-            <form action="" onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="">Poll Name</label>
-                    <input type="text" name="pollName" placeholder="" onChange={handleChange}/>
-                </div>
 
-                <div className="form-group">
-                    <label htmlFor="">Poll Description</label>
-                    <input type="text" name="pollDescription" onChange={handleChange}/>
-                </div>
+    // check if option.text is empty, if it is remove it from the state
 
-                <h2>Poll Options</h2>
-                <div>
-                    <button onClick={addOption}>Add Option</button> 
-                        {options.map((option, index) => {
-                            return (
-                                <div key={index} style={{marginTop:10}}>
-                                    <input 
-                                        placeholder="Type your option"
-                                        id={index}
-                                        onChange={(e) => handleOption(e)}
-                                        
-                                    />
-                                </div>
-                            )
-                        })}
-                    {/* <button onClick={addOption}>Add Option</button> */}
-                    
-                </div>
-                <button type="submit">submit form</button>
-            </form>
-            <div>
-                <p>Options List</p>
-                <ul>
-                    {/* {options.map((option) => <li>{option}</li>)} */}
-                </ul>
-            </div>
+    setOptions((prevOptions) => {
+      return prevOptions.filter((option) => {
+        Object.keys(option).length !== 0;
+      });
+      console.log(">>>>>", options);
+    });
+
+    // const reviewedOptions = options.filter((option) => option.text.trim() !=="")
+    // console.log(reviewedOptions)
+    // setOptions(reviewedOptions)
+    console.log(options);
+
+    const response = await fetch("http://localhost:4000/polls", {
+      method: "POST",
+      body: JSON.stringify({
+        title: polling.pollName,
+        description: polling.pollDescription,
+        options,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+
+    nav("/");
+  }
+  function addOption(e) {
+    e.preventDefault();
+    setOptions([...options, {}]);
+  }
+
+  async function handleDeleteOption(e, index) {
+    e.preventDefault();
+    console.log("option", options);
+    console.log("index:", index);
+
+    const updatedOption = options.filter((option, i) => {
+      console.log("i: ", i);
+      return i !== index;
+    });
+
+    setOptions([...updatedOption]);
+  }
+
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="">Poll Name</label>
+          <input
+            type="text"
+            name="pollName"
+            placeholder=""
+            onChange={handleChange}
+          />
         </div>
-    )
+
+        <div className="form-group">
+          <label htmlFor="">Poll Description</label>
+          <input type="text" name="pollDescription" onChange={handleChange} />
+        </div>
+
+        <h2>Poll Options</h2>
+        <div>
+          <button onClick={addOption}>Add Option</button>
+          {options.map((option, index) => {
+            return (
+              <div key={index} style={{ marginTop: 10 }}>
+                <input
+                  placeholder="Type your option"
+                  id={index}
+                  onChange={(e) => handleOption(e)}
+                />
+                <button
+                  id="deleteButton"
+                  onClick={(e) => handleDeleteOption(e, index)}
+                >
+                  Delete Option
+                </button>
+              </div>
+            );
+          })}
+          {/* <button onClick={addOption}>Add Option</button> */}
+        </div>
+        <button type="submit">submit form</button>
+      </form>
+    </div>
+  );
 }
 
-export default PollForm
+export default PollForm;
