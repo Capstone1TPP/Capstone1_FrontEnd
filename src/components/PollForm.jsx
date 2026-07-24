b5016701496e991d5cd4f8a037fb1878e7083dc2
YeffryF <yeffry.fermin.ttp@gmail.com>
2026-07-24 13:54:37 -0400

    feat: add way to store multiple polling options

--- src/components/PollForm.jsx
+++ src/components/PollForm.jsx
@@ -6,8 +6,12 @@ function PollForm () {
         pollName: '',
         pollDescription: ''
     })
-    const [options, setOptions] = useState([])
-    const [text, setText] = useState('')
+    const [options, setOptions] = useState([{text: ""}, {text: ""}])
+    const [optionVal, setOptionVal] = useState('')
+    const [error, setError] = useState({
+        optionOne: "Option 1 is required",
+        optionTwo: "Option 2 is required",
+    })
     const nav = useNavigate()
 
     function handleChange (e) {
@@ -18,8 +22,35 @@ function PollForm () {
             }
         })
     }
+    function handleOption(e) {
+        setOptions((prevOptions) => {
+            const inputIndex = Number(e.target.attributes["id"].value)
+
+            return prevOptions.map((option, index) => {
+                if(index === inputIndex) {
+                    return {
+                        text: e.target.value
+                    }
+                } else {
+                    return {
+                        ...option
+                    }
+                }
+            })
+        })
+    }
     async function handleSubmit(e) {
         e.preventDefault()
+        if(options[0].text === "") {
+            alert(error.optionOne)
+            return;
+        } 
+        if(options[1].text === "") {
+            alert(error.optionTwo)
+            return;
+        }
+
+        // check if option.text is empty, if it is remove it from the state
         const response = await fetch('http://localhost:4000/polls', {
             method: "POST",
             body: JSON.stringify({title: polling.pollName, description: polling.pollDescription, options}),
@@ -31,8 +62,7 @@ function PollForm () {
     }
     function addOption(e) {
         e.preventDefault()
-        setOptions([...options, text])
-        setText('')
+        setOptions([...options, {}])
     }
     return (
         <div>
@@ -49,12 +79,20 @@ function PollForm () {
 
                 <h2>Poll Options</h2>
                 <div>
-                    <input 
-                        placeholder="Type your option" 
-                        value={text} 
-                        onChange={(e) => setText(e.target.value)}
-                    />
-                    <button onClick={addOption}>Add Option</button>
+                    <button onClick={addOption}>Add Option</button> 
+                        {options.map((option, index) => {
+                            return (
+                                <div key={index} style={{marginTop:10}}>
+                                    <input 
+                                        placeholder="Type your option"
+                                        id={index}
+                                        onChange={(e) => handleOption(e)}
+                                        
+                                    />
+                                </div>
+                            )
+                        })}
+                    {/* <button onClick={addOption}>Add Option</button> */}
                     
                 </div>
                 <button type="submit">submit form</button>
@@ -62,7 +100,7 @@ function PollForm () {
             <div>
                 <p>Options List</p>
                 <ul>
-                    {options.map((option) => <li>{option}</li>)}
+                    {/* {options.map((option) => <li>{option}</li>)} */}
                 </ul>
             </div>
         </div>
